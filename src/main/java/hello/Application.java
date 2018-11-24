0891c6154c0daef9632ecbb20181b28d9e379b3e
Junfeng Dai <ryandjf@hotmail.com>
2018-11-25 07:14:32 +0800

    update

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World Too!";
+        return "Hello Docker World, the end!";
     }
 
     public static void main(String[] args) {
