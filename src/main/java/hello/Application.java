3ef827d2222bc67900ca1a5231c7fe753a90c38e
Junfeng Dai <ryandjf@hotmail.com>
2018-11-27 09:06:20 +0800

    final test?

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World, Welcome!";
+        return "Hello Docker World, Finally!";
     }
 
     public static void main(String[] args) {
